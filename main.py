74566790a1193e47daa45b5de5f0a97924ea4ac1
Jonny Saykosy <josayko@pm.me>
2023-06-17 10:34:15 +0200

    algo done

--- main.py
+++ main.py
@@ -12,9 +12,26 @@ for typ in data["Unnamed: 0"].values():
         break
     typs.append(typ)
 
-print(typs)
+# print(typs)
+
+res = {}
 
 for col in range(1, len(data) - 1):
-    print(f'Lot {col}: ', list(data[col].values())[:len(typs)])
+    vals = list(data[col].values())[:len(typs)]
+    comb = tuple(zip(typs, vals))
+    if comb not in res:
+        res[comb] = [str(col)]
+    else:
+        res[comb].append(str(col))
 
+# print(res)
+
+for key in res.keys():
+    types = []
+    for t in key:
+        if (t[1]):
+            types.append(t[0])
+
+    if types:
+        print(f"LOT={','.join(res[key])};TYP={','.join(types)}")
 
