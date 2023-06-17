318c852440f62de8cc7e43c1b8be8a037d6cce3c
Jonny Saykosy <josayko@pm.me>
2023-06-17 10:52:35 +0200

    update script with main

--- main.py
+++ main.py
@@ -1,37 +1,43 @@
+import sys
 import pandas as pd
 
-df = pd.read_excel(
-    "Copie de Matrice des VISA à compléter  BET SEDRI - CFO-GTB.xlsx", header=1
-).fillna("")
+def parse(filename):
+    df = pd.read_excel(
+        filename, header=1
+    ).fillna("")
 
-data = df.to_dict()
-typs = []
+    data = df.to_dict()
+    typs = []
 
-for typ in data["Unnamed: 0"].values():
-    if not typ:
-        break
-    typs.append(typ)
+    for typ in data["Unnamed: 0"].values():
+        if not typ:
+            break
+        typs.append(typ)
 
-# print(typs)
+    # print(typs)
 
-res = {}
+    res = {}
 
-for col in range(1, len(data) - 1):
-    vals = list(data[col].values())[:len(typs)]
-    comb = tuple(zip(typs, vals))
-    if comb not in res:
-        res[comb] = [str(col)]
-    else:
-        res[comb].append(str(col))
+    for col in range(1, len(data) - 1):
+        vals = list(data[col].values())[:len(typs)]
+        comb = tuple(zip(typs, vals))
+        if comb not in res:
+            res[comb] = [str(col)]
+        else:
+            res[comb].append(str(col))
 
-# print(res)
+    # print(res)
 
-for key in res.keys():
-    types = []
-    for t in key:
-        if (t[1]):
-            types.append(t[0])
+    for key in res.keys():
+        types = []
+        for t in key:
+            if (t[1]):
+                types.append(t[0])
 
-    if types:
-        print(f"LOT={','.join(res[key])};TYP={','.join(types)}")
+        if types:
+            print(f"LOT={','.join(res[key])};TYP={','.join(types)}")
 
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        parse(sys.argv[1])
