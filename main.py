4dc9ecefe93d9817040eabd4b87eea8a563c32f4
Jonny Saykosy <josayko@pm.me>
2023-06-17 15:04:28 +0200

    TODO: write in file

--- main.py
+++ main.py
@@ -3,49 +3,62 @@ import re
 import pandas as pd
 
 
-def parse(file_name, sheet_name=0, row_type="TYP"):
-    df = pd.read_excel(file_name, sheet_name, header=1).fillna("")
-
-    df_dic = df.to_dict()
-    typs = []
-
-    for typ in df_dic["Unnamed: 0"].values():
-        if not typ:
-            break
-        typs.append(typ)
-
-    # print(typs)
-    regex = re.compile(r"^Unnamed.*")
-    data = {
-        str(key): value for key, value in df_dic.items() if not regex.match(str(key))
-    }
-    res = {}
-
-    for key in data:
-        vals = list(data[key].values())[: len(typs)]
-        comb = tuple(zip(typs, vals))
-        if comb not in res:
-            res[comb] = [str(key)]
-        else:
-            res[comb].append(str(key))
-
-    # print(res)
-
+def compute_results(res):
     for key in res.keys():
         types = []
         for t in key:
             if t[1]:
                 types.append(t[0])
-
         if types:
-            if row_type == "TYP":
-                lots = f"LOT={','.join(res[key])}"
-                types = f"TYP={','.join(types)}"
-            else:
-                lots = f"LOT={','.join(types)}"
-                types = f"TYP={','.join(res[key])}"
+            print(f"LOT={','.join(res[key])};TYP={','.join(types)}")
 
-            print(f"{lots};{types}")
+
+def parse_lot_column(rows, data):
+    res = {}
+    for key in data:
+        vals = list(data[key].values())[: len(rows)]
+        comb = tuple(zip(rows, vals))
+        if comb not in res:
+            res[comb] = [str(key)]
+        else:
+            res[comb].append(str(key))
+
+    compute_results(res)
+
+
+def parse_lot_row(rows, data):
+    res = {}
+    col_vals = [list(x.values()) for x in data.values()]
+    row_vals = zip(*col_vals)
+
+    for index, vals in enumerate(row_vals):
+        comb = tuple(zip(data.keys(), vals))
+        if comb not in res:
+            res[comb] = [str(rows[index])]
+        else:
+            res[comb].append(str(rows[index]))
+
+    compute_results(res)
+
+
+def parse(file_name, sheet_name=0, lot_direction="col"):
+    df = pd.read_excel(file_name, sheet_name, header=1).fillna("")
+    df_dic = df.to_dict()
+    rows = []
+
+    for typ in df_dic["Unnamed: 0"].values():
+        if not typ:
+            break
+        rows.append(typ)
+
+    regex = re.compile(r"^Unnamed.*")
+    data = {
+        str(key): value for key, value in df_dic.items() if not regex.match(str(key))
+    }
+    if lot_direction.lower() == "row":
+        parse_lot_row(rows, data)
+    else:
+        parse_lot_column(rows, data)
 
 
 if __name__ == "__main__":
