0c07e1cfe372a657d28da1d0bf6e95f97e50a40e
Jonny Saykosy <josayko@pm.me>
2023-06-17 12:22:07 +0200

    invert TYP / LOT

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ import sys
 import re
 import pandas as pd
 
-def parse(file_name, sheet_name=0, row_type="LOT"):
-    df = pd.read_excel(
-        file_name, sheet_name, header=1
-    ).fillna("")
+
+def parse(file_name, sheet_name=0, row_type="TYP"):
+    df = pd.read_excel(file_name, sheet_name, header=1).fillna("")
 
     df_dic = df.to_dict()
     typs = []
@@ -17,11 +16,13 @@ def parse(file_name, sheet_name=0, row_type="LOT"):
 
     # print(typs)
     regex = re.compile(r"^Unnamed.*")
-    data = {str(key): value for key, value in df_dic.items() if not regex.match(str(key))}
+    data = {
+        str(key): value for key, value in df_dic.items() if not regex.match(str(key))
+    }
     res = {}
 
     for key in data:
-        vals = list(data[key].values())[:len(typs)]
+        vals = list(data[key].values())[: len(typs)]
         comb = tuple(zip(typs, vals))
         if comb not in res:
             res[comb] = [str(key)]
@@ -33,11 +34,11 @@ def parse(file_name, sheet_name=0, row_type="LOT"):
     for key in res.keys():
         types = []
         for t in key:
-            if (t[1]):
+            if t[1]:
                 types.append(t[0])
 
         if types:
-            if row_type == "LOT":
+            if row_type == "TYP":
                 lots = f"LOT={','.join(res[key])}"
                 types = f"TYP={','.join(types)}"
             else:
@@ -50,4 +51,3 @@ def parse(file_name, sheet_name=0, row_type="LOT"):
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         parse(*sys.argv[1:])
-
