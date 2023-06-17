fe441a89d884eb4dcf42900f7c39b4b188e55fdb
Jonny Saykosy <josayko@pm.me>
2023-06-17 11:55:25 +0200

    update

--- main.py
+++ main.py
@@ -1,30 +1,32 @@
 import sys
+import re
 import pandas as pd
 
-def parse(filename):
+def parse(file_name, sheet_name=0, row_type="LOT"):
     df = pd.read_excel(
-        filename, header=1
+        file_name, sheet_name, header=1
     ).fillna("")
 
-    data = df.to_dict()
+    df_dic = df.to_dict()
     typs = []
 
-    for typ in data["Unnamed: 0"].values():
+    for typ in df_dic["Unnamed: 0"].values():
         if not typ:
             break
         typs.append(typ)
 
     # print(typs)
-
+    regex = re.compile(r"^Unnamed.*")
+    data = {str(key): value for key, value in df_dic.items() if not regex.match(str(key))}
     res = {}
 
-    for col in range(1, len(data) - 1):
-        vals = list(data[col].values())[:len(typs)]
+    for key in data:
+        vals = list(data[key].values())[:len(typs)]
         comb = tuple(zip(typs, vals))
         if comb not in res:
-            res[comb] = [str(col)]
+            res[comb] = [str(key)]
         else:
-            res[comb].append(str(col))
+            res[comb].append(str(key))
 
     # print(res)
 
@@ -35,9 +37,17 @@ def parse(filename):
                 types.append(t[0])
 
         if types:
-            print(f"LOT={','.join(res[key])};TYP={','.join(types)}")
+            if row_type == "LOT":
+                lots = f"LOT={','.join(res[key])}"
+                types = f"TYP={','.join(types)}"
+            else:
+                lots = f"LOT={','.join(types)}"
+                types = f"TYP={','.join(res[key])}"
+
+            print(f"{lots};{types}")
 
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        parse(sys.argv[1])
+        parse(*sys.argv[1:])
+
