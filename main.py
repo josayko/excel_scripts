c290922018a519168c369a17bf7aec627cd0c539
Jonny Saykosy <josayko@pm.me>
2025-03-31 13:24:06 +0200

    broken: testing

--- main.py
+++ main.py
@@ -2,46 +2,50 @@ import os
 
 os.environ["PYTHON_WARNINGS"] = "ignore::UserWarning"
 
-from tkinter import TOP, Checkbutton, Entry, IntVar, Label, StringVar
-from tkinterdnd2 import TkinterDnD, DND_ALL
+import tkinter as tk
+from tkinter import TOP, Checkbutton, Entry, IntVar, StringVar
+from tkinterdnd2 import TkinterDnD, DND_FILES
 
 from matrix_parser import parser
 
 
 def get_path(event):
-    temp = event.data[1:-1]
+    if lb.size() >= 2:
+        lb.delete(1, lb.size() - 1)
+
+    temp = event.data
     tokens = temp.split("} {")
-    label.configure(text=tokens)
+    lb.insert(tk.END, event.data)
     printToFile = ""
 
-    for path in tokens:
-        var = StringVar()
-        results = parser.parse(path, "typ")
-        formatted = ""
-        for result in results.items():
-            formatted = formatted + "\n" + result[0] + "\n" + result[1] + "\n"
-        var.set(formatted)
-        resultsWidget.configure(textvariable=var)
-        printToFile = (
-            printToFile + "\n------------------------------\n" + path + var.get()
-        )
-        print(path, var.get())
+    # for path in tokens:
+    #     var = StringVar()
+    #     results = parser.parse(path, "typ")
+    #     formatted = ""
+    #     for result in results.items():
+    #         formatted = formatted + "\n" + result[0] + "\n" + result[1] + "\n"
+    #     var.set(formatted)
+    #     resultsWidget.configure(textvariable=var)
+    #     printToFile = (
+    #         printToFile + "\n------------------------------\n" + path + var.get()
+    #     )
+    #     print(path, var.get())
 
     with open("results.txt", "w") as f:
         f.write(printToFile)
+    # Open the file in macos
+    os.system("open results.txt")
 
 
 window = TkinterDnD.Tk()
-window.geometry("350x150")
+window.geometry("500x300")
 window.title("SOFYA tools - Matrix reader")
 
 nameVar = StringVar()
 
-entryWidget = Entry(window)
-entryWidget.pack(side=TOP, padx=5, pady=5)
-
-label = Label(window, text="Drag and drop files in the entry box")
-label.pack(side=TOP)
+lb = tk.Listbox(window)
+lb.insert(1, "Drag and drop your file here : ")
+lb.pack(side=TOP, padx=5, pady=5)
 
 var = StringVar()
 var.set("Some text")
@@ -52,7 +56,10 @@ var1 = IntVar(value=1)
 c1 = Checkbutton(window, text="typ", variable=var1, onvalue=1, offvalue=0)
 c1.pack()
 
-entryWidget.drop_target_register(DND_ALL)  # type: ignore
-entryWidget.dnd_bind("<<Drop>>", get_path)  # type: ignore
+lb.drop_target_register(DND_FILES)  # type: ignore
+lb.dnd_bind("<<Drop>>", lambda e: get_path(e))  # type: ignore
+
+# entryWidget.drop_target_register(DND_ALL)  # type: ignore
+# entryWidget.dnd_bind("<<Drop>>", get_path)  # type: ignore
 
 window.mainloop()
