386446284dfc259514a31c9d166e5b70f5c2b9ff
Jonny Saykosy <josayko@pm.me>
2025-07-03 11:33:43 +0200

    fix: broken file parser

--- main.py
+++ main.py
@@ -4,32 +4,36 @@ os.environ["PYTHON_WARNINGS"] = "ignore::UserWarning"
 
 import tkinter as tk
 from tkinter import TOP, Checkbutton, Entry, IntVar, StringVar
-from tkinterdnd2 import TkinterDnD, DND_FILES
+
+from tkinterdnd2 import DND_FILES, TkinterDnD
 
 from matrix_parser import parser
 
 
-def get_path(event):
+def get_path(window, event):
     if lb.size() >= 2:
         lb.delete(1, lb.size() - 1)
 
-    temp = event.data
-    tokens = temp.split("} {")
+    file_list = window.tk.splitlist(event.data)
+    file_list = [file_list[0]]
     lb.insert(tk.END, event.data)
     printToFile = ""
 
-    # for path in tokens:
-    #     var = StringVar()
-    #     results = parser.parse(path, "typ")
-    #     formatted = ""
-    #     for result in results.items():
-    #         formatted = formatted + "\n" + result[0] + "\n" + result[1] + "\n"
-    #     var.set(formatted)
-    #     resultsWidget.configure(textvariable=var)
-    #     printToFile = (
-    #         printToFile + "\n------------------------------\n" + path + var.get()
-    #     )
-    #     print(path, var.get())
+    for path in file_list:
+        var = StringVar()
+        resultsWidget.configure(textvariable=var)
+        try:
+            results = parser.parse(path, "typ")
+            formatted = ""
+            for result in results.items():
+                formatted = formatted + "\n" + result[0] + "\n" + result[1] + "\n"
+            var.set(formatted)
+        except Exception:
+            var.set(str("Error : the file is not well formatted"))
+        printToFile = (
+            printToFile + "\n------------------------------\n" + path + var.get()
+        )
+        print(path, var.get())
 
     with open("results.txt", "w") as f:
         f.write(printToFile)
@@ -53,13 +57,12 @@ resultsWidget = Entry(window, state="readonly", width=100)
 resultsWidget.pack(side=TOP)
 
 var1 = IntVar(value=1)
-c1 = Checkbutton(window, text="typ", variable=var1, onvalue=1, offvalue=0)
+c1 = Checkbutton(
+    window, text="Colonnes=LOT / Lignes=TYP", variable=var1, onvalue=1, offvalue=0
+)
 c1.pack()
 
 lb.drop_target_register(DND_FILES)  # type: ignore
-lb.dnd_bind("<<Drop>>", lambda e: get_path(e))  # type: ignore
-
-# entryWidget.drop_target_register(DND_ALL)  # type: ignore
-# entryWidget.dnd_bind("<<Drop>>", get_path)  # type: ignore
+lb.dnd_bind("<<Drop>>", lambda e: get_path(window, e))  # type: ignore
 
 window.mainloop()
